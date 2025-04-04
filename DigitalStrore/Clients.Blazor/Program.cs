20b2ec6b63822948d18e6d356033cb50037efab1
eladbalva <eladbalva@gmail.com>
2025-04-04 20:00:52 +0300

    Init Project

---
+++ DigitalStrore/Clients.Blazor/Program.cs
@@ -0,0 +1,39 @@
+using Clients.Blazor.Components;
+using Clients.Blazor.Services;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+
+builder.Services.AddHttpClient();
+builder.Services.AddSingleton<ClientsService>();
+builder.Services.AddSingleton<CityService>();
+builder.Services.AddSingleton<CompanyService>();
+builder.Services.AddSingleton<CategoryService>();
+builder.Services.AddSingleton<ProductService>();
+builder.Services.AddSingleton<FileService>();
+builder.Services.AddSingleton<OrderService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+app.Run();
